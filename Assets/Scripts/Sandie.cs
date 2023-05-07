79baf9a7e5e3c3e365f99fa9578db09bba34c275
Lucas Cohen <lucohen@chapman.edu>
2023-05-06 21:26:12 -0700

    More stuff

--- Assets/Scripts/Sandie.cs
+++ Assets/Scripts/Sandie.cs
@@ -21,8 +21,8 @@ public class Sandie : Character
 
     public void Update()
     {
-        if (HasGameJustEnded())
-            ResetSandie();
+       // if (HasGameJustEnded())
+         //   ResetSandie();
 
     }
 
